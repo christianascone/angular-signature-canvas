56858cf4653933eeb96f875c9bf774a721242ef0
Christian Ascone <ascone.christian@gmail.com>
2016-07-28 23:42:05 +0200

    Set main src file with module definition

---
+++ src/angular-signature-pad.js
@@ -0,0 +1,12 @@
+/**!
+ * Angular JS Directive for the szimek's signature pad
+ * @author Christian Ascone <ascone.christian@gmail.com>
+ * @version 0.0.1
+ *
+ * https://github.com/christianascone/angular-signature-pad
+ * Copyright (c) 2016 ; Licensed MIT
+ */
+
+angular.module('angular-signature-pad', []);
+
+angular.module('angular-signature-pad').directive('signaturePad');
