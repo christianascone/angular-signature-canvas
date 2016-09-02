3063a94455d38008f01a20c5844aa4291858d9dd
ChristianAscone <ascone.christian@gmail.com>
2016-09-02 10:36:16 +0200

    Signature points added to array

--- src/angular-signature-canvas.js
+++ src/angular-signature-canvas.js
@@ -14,31 +14,89 @@ angular.module('angular-signature-canvas').controller("signaturePadController", 
   var idContainer = "signature-pad";
   var idCanvas = "signatureCanvas";
   var margin = 10;
+  ctrl.points = [];
+  ctrl.canvasSignature = ctrl.canvasSignature || {};
 
+  /**
+   * Point object definition with velocityFrom() and distanceTo() functions.
+   * Both the constructor and the prototype functions are a simple copy
+   * of szimek original class
+   * https://github.com/szimek/signature_pad
+   */
+  var Point = function(x, y, time) {
+    this.x = x;
+    this.y = y;
+    this.time = time || new Date().getTime();
+  };
+  Point.prototype.velocityFrom = function(start) {
+    return (this.time !== start.time) ? this.distanceTo(start) / (this.time - start.time) : 1;
+  };
+  Point.prototype.distanceTo = function(start) {
+    return Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
+  };
+
+  /**
+   * Clears the signature canvas and the points array
+   * 
+   * @return {void}
+   */
   ctrl.clear = function() {
     ctrl.canvasSignature.clear();
+    ctrl.points = [];
+  }
+
+  ctrl.addSignaturePoint = function(event) {
+    var point = ctrl.createPoint(event, ctrl.canvasSignature._canvas);
+    ctrl.points.push(point);
+  }
+
+  /**
+   * Create a point using the given event (for the point coordinate)
+   * and the canvas for bounding client rectangle
+   * 
+   * @param  {event}
+   * @param  {canvas}
+   * @return {Point}
+   */
+  ctrl.createPoint = function(event, canvas) {
+    var rect = canvas.getBoundingClientRect();
+    return new Point(
+      event.clientX - rect.left,
+      event.clientY - rect.top
+    );
   }
 
   ctrl.init = function() {
     var container = document.getElementById(idContainer);
     var canvas = document.getElementById(idCanvas);
-    canvas.style.margin = margin+"px";
-    
-    container.style.width = ctrl.width+"px";
-    canvas.style.width = ctrl.width+"px";
+    canvas.style.margin = margin + "px";
+
+    container.style.width = ctrl.width + "px";
+    canvas.style.width = ctrl.width + "px";
     canvas.width = ctrl.width;
-    container.style.height = ctrl.height+"px";
-    canvas.style.height = ctrl.height+"px";
+    container.style.height = ctrl.height + "px";
+    canvas.style.height = ctrl.height + "px";
     canvas.height = ctrl.height;
 
-    ctrl.canvasSignature = new SignaturePad(canvas);
+    ctrl.canvasSignature = new SignaturePad(canvas, {
+      // Callback function called every time the signature starts
+      onBegin: function(event) {ctrl.addSignaturePoint(event)},
+      // Callback function called every time the signature ends
+      onEnd: function(event) {ctrl.addSignaturePoint(event)}
+    });
+    // Add listener for callback function to be called when the mouse moves (during signature)
+    ctrl.canvasSignature._canvas.addEventListener("mousemove", function(event) {
+      if (ctrl.canvasSignature._mouseButtonDown) {
+        ctrl.addSignaturePoint(event);
+      }
+    });
     $scope.canvas = canvas;
 
     // Check width of the parent node in order to init the pad
     // only when the area is ready
     $scope.$watch('canvas.parentNode.offsetWidth',
       function(newValue, oldValue) {
-        if(newValue > 0 && newValue != oldValue){
+        if (newValue > 0 && newValue != oldValue) {
           ctrl.init();
         }
       }, //listener 
@@ -49,7 +107,7 @@ angular.module('angular-signature-canvas').controller("signaturePadController", 
       var canvas = document.getElementById(idCanvas);
 
       // If canvas is not available yet, it stops
-      if(!canvas){
+      if (!canvas) {
         return;
       }
 
@@ -57,8 +115,8 @@ angular.module('angular-signature-canvas').controller("signaturePadController", 
       var offsetHeight = canvas.offsetHeight;
 
       var canvasParent = canvas.parentNode;
-      
-      var width = canvasParent.offsetWidth - (margin*2);
+
+      var width = canvasParent.offsetWidth - (margin * 2);
       canvas.width = width;
       canvas.style.width = width;
 
